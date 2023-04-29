f82c5f8a017858c6de0669831a746ac505b1e5bb
AlexB <6y6jlb@gmail.com>
2023-04-29 08:29:02 +0400

    report refactoring

--- components/report/Report.js
+++ components/report/Report.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Row, Rows, Table, TableWrapper, Cell } from 'react-native-table-component';
+import { StyleSheet, View, ScrollView} from 'react-native';
+import { Row, Rows, Table, TableWrapper, Cell,  } from 'react-native-table-component';
 
 
 export default ({data}) => {
+  
 
   return (
     <View>
+        <ScrollView horizontal>
         <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
           <Row data={data.tableHead} style={styles.head} textStyle={styles.text}/>
           {
@@ -14,20 +16,21 @@ export default ({data}) => {
               <TableWrapper key={index} style={styles.row}>
                 {
                   rowData.map((cellData, cellIndex) => (
-                    <Cell key={cellIndex} data={cellData} textStyle={styles.text}/>
+                    <Cell key={cellIndex} data={cellData} textStyle={[styles.text]}/>
                   ))
                 }
               </TableWrapper>
             ))
           }
         </Table>
+        </ScrollView>
       </View>
   )
 }
 
 
 const styles = StyleSheet.create({
-    head: { height: 40, backgroundColor: '#f1f8ff' },
+    head: { height: 40, backgroundColor: '#f1f8ff', width: 'max-content' },
     text: { margin: 6 },
     row: { flexDirection: 'row', backgroundColor: '#FFF1C1' },
   });
