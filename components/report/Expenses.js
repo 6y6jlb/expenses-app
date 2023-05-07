517f532b2064e308f22a5d8b4752893b17c39cc1
AlexB <6y6jlb@gmail.com>
2023-05-07 15:03:28 +0400

    handlers in progress

--- components/report/Expenses.js
+++ components/report/Expenses.js
@@ -1,29 +1,38 @@
-import React, {memo} from "react"
-import { ScrollView, View } from "react-native"
+import React, { memo } from "react"
+import { ScrollView, View, TouchableOpacity, Text } from "react-native"
 import { Cell, Col, Row, Table, TableWrapper } from "react-native-table-component"
 import NoData from "../noData/NoData"
 import { styles } from "./styles"
-
 
 const Expenses = ({ data }) => {
 	if (data.tableHead.length < 1) {
 		return <NoData />
 	}
 
+	const getTitle = (data, index) => (
+		<TouchableOpacity onPress={() => console.log(index)}>
+			<Text style={[styles.text, styles.selfCenter]}>{data}</Text>
+		</TouchableOpacity>
+	)
+
 	return (
-		<ScrollView>
+		<ScrollView horizontal>
 			<View style={styles.container}>
 				<Table style={{ flexDirection: "row" }} borderStyle={{ borderWidth: 2, borderColor: "#c8e1ff" }}>
 					<TableWrapper style={{ width: 90 }}>
-						<Cell data={["id"]} style={[styles.singleHead]} />
-						<Col data={data.tableTitle} textStyle={[styles.text]} />
+						<Cell data={["id"]} style={[styles.singleHead]} textStyle={styles.selfCenter} />
+						<TableWrapper style={styles.column}>
+							{data.tableTitle.map((cellData, cellIndex) => (
+								<Cell key={cellIndex} data={cellData} />
+							))}
+						</TableWrapper>
 					</TableWrapper>
 					<TableWrapper style={{ flex: 1 }}>
-						<Row data={data.tableHead} style={styles.head} textStyle={styles.text} />
+						<Row data={data.tableHead} style={[styles.head,]} textStyle={[styles.text, styles.cell]} />
 						{data.tableData.map((rowData, index) => (
 							<TableWrapper key={index} style={styles.row}>
 								{rowData.map((cellData, cellIndex) => (
-									<Cell key={cellIndex} data={cellData} textStyle={[styles.text]} />
+									<Cell key={cellIndex} data={cellData}  textStyle={[styles.text, styles.cell]} />
 								))}
 							</TableWrapper>
 						))}
--- components/report/styles.js
+++ components/report/styles.js
@@ -4,6 +4,11 @@ export const styles = StyleSheet.create({
 	container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: "#fff" },
 	head: { height: 40, backgroundColor: "#f1f8ff" },
 	singleHead: { width: 90, height: 40, backgroundColor: "#c8e1ff" },
-	text: { margin: 6 },
+	text: { margin: 6, },
+	cell: {
+		width: 100,
+	},
 	row: { flexDirection: "row", backgroundColor: "#FFF1C1" },
+	column: { flexDirection: "column", backgroundColor: "#f1f8ff" },
+	selfCenter: { alignSelf: "center" },
 })
