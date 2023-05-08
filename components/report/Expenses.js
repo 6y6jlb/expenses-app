bb3931cb4f19c681c671af85de3918095366b093
AlexB <6y6jlb@gmail.com>
2023-05-08 11:50:30 +0400

    scroll fixed

--- components/report/Expenses.js
+++ components/report/Expenses.js
@@ -11,8 +11,8 @@ const Expenses = ({ data, onChange }) => {
 
 	const arrWidth = data.tableHead.map((el, index) => {
 		if (index === 0) return 100
-		if(data.tableData[index].length <= el.length) return el.length * 12 + 5
-		if(data.tableData[index].length >= el.length) return data.tableData[index].length * 10 + 5
+		if (data.tableData[index].length <= el.length) return el.length * 12 + 5
+		if (data.tableData[index].length >= el.length) return data.tableData[index].length * 10 + 5
 		return 100
 	})
 
@@ -23,42 +23,48 @@ const Expenses = ({ data, onChange }) => {
 	)
 
 	return (
-		<ScrollView horizontal>
-			<View style={styles.container}>
-				<Table style={{ flexDirection: "row" }} borderStyle={{ borderWidth: 2, borderColor: "#c8e1ff" }}>
-					<TableWrapper style={{ width: 90 }}>
-						<Cell data={["id"]} style={[styles.singleHead]} textStyle={[styles.selfCenter, styles.text]} />
-						{data.tableTitle.map((rowData, index) => (
+		<ScrollView>
+			<ScrollView horizontal>
+				<View style={styles.container}>
+					<Table style={{ flexDirection: "row" }} borderStyle={{ borderWidth: 2, borderColor: "#c8e1ff" }}>
+						<TableWrapper style={{ width: 90 }}>
 							<Cell
-								key={index}
-								data={element(rowData, index)}
-								style={styles.column}
-								textStyle={styles.text}
+								data={["id"]}
+								style={[styles.singleHead]}
+								textStyle={[styles.selfCenter, styles.text]}
 							/>
-						))}
-					</TableWrapper>
-					<TableWrapper style={{ flex: 1 }}>
-						<Row
-							data={data.tableHead}
-							style={[styles.head]}
-							widthArr={arrWidth}
-							textStyle={[styles.text]}
-						/>
-						{data.tableData.map((rowData, index) => (
-							<TableWrapper key={index} style={styles.row}>
-								{rowData.map((cellData, cellIndex) => (
-									<Cell
-										key={cellIndex}
-										width={arrWidth[cellIndex]}
-										data={cellData}
-										textStyle={[styles.text]}
-									/>
-								))}
-							</TableWrapper>
-						))}
-					</TableWrapper>
-				</Table>
-			</View>
+							{data.tableTitle.map((rowData, index) => (
+								<Cell
+									key={index}
+									data={element(rowData, index)}
+									style={styles.column}
+									textStyle={styles.text}
+								/>
+							))}
+						</TableWrapper>
+						<TableWrapper style={{ flex: 1 }}>
+							<Row
+								data={data.tableHead}
+								style={[styles.head]}
+								widthArr={arrWidth}
+								textStyle={[styles.text]}
+							/>
+							{data.tableData.map((rowData, index) => (
+								<TableWrapper key={index} style={styles.row}>
+									{rowData.map((cellData, cellIndex) => (
+										<Cell
+											key={cellIndex}
+											width={arrWidth[cellIndex]}
+											data={cellData}
+											textStyle={[styles.text]}
+										/>
+									))}
+								</TableWrapper>
+							))}
+						</TableWrapper>
+					</Table>
+				</View>
+			</ScrollView>
 		</ScrollView>
 	)
 }
